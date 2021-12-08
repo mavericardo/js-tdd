f7904c825f181a4c6af148dbc82c4ad89fa2f02b
Marcos Ricardo <mricardoalves99@gmail.com>
2021-12-08 18:28:58 -0300

    refactor: remove unused function

--- src/main.js
+++ src/main.js
@@ -3,14 +3,6 @@ const sub = (num1, num2) => num1 - num2;
 const mult = (num1, num2) => num1 * num2;
 const div = (num1, num2) => ((num2 === 0) ? "Não é possível divisão por zero!" : (num1 / num2));
 
-function bla() {
-  console.log("bla");
-}
-
-function hello() {
-  console.log("hello");
-}
-
 export {
   sum,
   sub,
